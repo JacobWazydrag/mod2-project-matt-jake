91a5723a619becb6daa79e97d37ea55648bff3fd
J-Waz <jgw.jakegeorge@gmail.com>
2020-07-27 14:51:15 -0500

    added relationship between user and chirps

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_194313) do
+ActiveRecord::Schema.define(version: 2020_07_27_195017) do
 
   create_table "chirps", force: :cascade do |t|
     t.text "chirp"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
