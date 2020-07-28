6e507152168319f7102f5c66fa39cbc29e9b37d4
J-Waz <jgw.jakegeorge@gmail.com>
2020-07-28 12:12:38 -0500

    adding likes

--- db/migrate/20200727132430_create_chirps.rb
+++ db/migrate/20200727132430_create_chirps.rb
@@ -1,7 +1,7 @@
 class CreateChirps < ActiveRecord::Migration[6.0]
   def change
     create_table :chirps do |t|
-      t.integer :author_id
+      t.integer :user_id
       t.text :chirp_text
       t.text :image_url
 
--- db/migrate/20200728024244_create_images.rb
+++ db/migrate/20200728024244_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
-      t.integer :owner_id
+      t.integer :user_id
       t.text :img_url
 
       t.timestamps
