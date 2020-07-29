1873cbe517a96cce24a6613072d5dfc24de01464
msguttman <msguttman@gmail.com>
2020-07-28 22:07:20 -0500

    small update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do
-    User.create()
+#20.times do
+#   User.create()
