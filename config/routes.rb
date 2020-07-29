d15160a68e096f1a8b0c284cbe5e807f3e6c8d54
msguttman <msguttman@gmail.com>
2020-07-29 10:16:41 -0500

    user profile path

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :users
+
   resources :chirps
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#show'
+  root 'users#userprofile'
 
 end
-
