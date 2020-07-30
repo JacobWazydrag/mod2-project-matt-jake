26668744ce711f363151162e9ee96640efb0ee6a
J-Waz <jgw.jakegeorge@gmail.com>
2020-07-30 15:39:56 -0500

    username, name now required at signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :friendships
 
 
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
 
   resources :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_172800) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
